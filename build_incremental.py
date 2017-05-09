84a4d35c8d571e025f59c63b3ff7ebf8bcce5c52
Graydon Hoare <ghoare@apple.com>
2017-05-09 10:27:48 -0700

    Add build_incremental.py driver.

---
+++ build_incremental.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# ===--- build_incremental.py ---------------------------------------------===
+#
+#  This source file is part of the Swift.org open source project
+#
+#  Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+#  Licensed under Apache License v2.0 with Runtime Library Exception
+#
+#  See https://swift.org/LICENSE.txt for license information
+#  See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+# ===----------------------------------------------------------------------===
+
+"""Build a collection of Swift projects in incremental mode, collecting stats."""
+
+import argparse
+import json
+import sys
+
+import common
+import project
+
+
+def parse_args():
+    """Return parsed command line arguments."""
+    parser = argparse.ArgumentParser()
+    project.add_arguments(parser)
+    return parser.parse_args()
+
+
+def main():
+    """Execute specified indexed project actions."""
+    args = parse_args()
+    index = json.loads(open(args.projects).read())
+    result = project.ProjectListBuilder(
+        args.include_repos,
+        args.exclude_repos,
+        args.verbose,
+        project.ProjectBuilder.factory(
+            args.include_actions,
+            args.exclude_actions,
+            args.verbose,
+            project.IncrementalActionBuilder.factory(
+                args.swiftc,
+                args.swift_version,
+                args.swift_branch,
+                args.sandbox_profile_xcodebuild,
+                args.sandbox_profile_package,
+                args.add_swift_flags,
+                args.check_stats,
+                args.show_stats
+            ),
+        ),
+        index
+    ).build()
+    common.debug_print(str(result))
+    return 0 if result.result in [project.ResultEnum.PASS,
+                                  project.ResultEnum.XFAIL] else 1
+
+if __name__ == '__main__':
+    sys.exit(main())
