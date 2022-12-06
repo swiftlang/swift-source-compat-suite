312aaa28d55b78044d9864853f5672d3114c2f47
Yim Lee <yim_lee@apple.com>
2022-12-06 14:28:06 -0800

    Bump swift-crypto to v2.2.2
    To include https://github.com/apple/swift-crypto/pull/136

--- common.py
+++ common.py
@@ -40,7 +40,7 @@ branches = {
         'swift-driver': 'main',
         'yams': '4.0.2',
         'swift-tools-support-core': 'main',
-        'swift-crypto': '2.2.1',
+        'swift-crypto': '2.2.2',
         'swift-atomics': '1.0.2',
         'swift-collections': '1.0.1',
         'swift-numerics': '1.0.1',
